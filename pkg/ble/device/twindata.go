65497c5569d0f3533a04d13a5bc1bb10a7c1b202
ttlv <gogophersteven@gmail.com>
2021-04-08 12:37:12 +0800

    feat/twin: suuport read data actively

--- pkg/ble/device/twindata.go
+++ pkg/ble/device/twindata.go
@@ -30,10 +30,23 @@ func (td *TwinData) Run() {
 	if p, err := td.BluetoothClient.Client.DiscoverProfile(true); err == nil {
 		if u := p.Find(ble.NewCharacteristic(uuid)); u != nil {
 			c := u.(*ble.Characteristic)
+			// read data actively
+			if (c.Property & ble.CharRead) != 0 {
+				b, err := td.BluetoothClient.Client.ReadCharacteristic(c)
+				if err != nil {
+					klog.Errorf("Failed to read characteristic: %s\n", err)
+					return
+				}
+				td.Result = fmt.Sprintf("%f", td.ConvertReadData(b))
+				if err = td.handlerPublish(); err != nil {
+					klog.Error("publish data to mq failed")
+					return
+				}
+			}
 			// If this Characteristic suports notifications and there's a CCCD
 			// Then subscribe to it
 			if (c.Property&ble.CharNotify) != 0 && c.CCCD != nil {
-				if err := td.BluetoothClient.Client.Subscribe(c, false, td.handlerPublisher()); err != nil {
+				if err := td.BluetoothClient.Client.Subscribe(c, false, td.notificationHandler()); err != nil {
 					klog.Error(err)
 				}
 			}
@@ -41,32 +54,39 @@ func (td *TwinData) Run() {
 	}
 }
 
-func (td *TwinData) handlerPublisher() func(req []byte) {
+func (td *TwinData) notificationHandler() func(req []byte) {
 	return func(req []byte) {
 		td.Result = fmt.Sprintf("%f", td.ConvertReadData(req))
-		// construct payload
-		var payload []byte
-		var err error
-		if strings.Contains(td.Topic, "$hw") {
-			if payload, err = common.CreateMessageTwinUpdate(td.Name, td.Type, td.Result); err != nil {
-				klog.Error("Create message twin update failed")
-				return
-			}
-		} else {
-			if payload, err = common.CreateMessageData(td.Name, td.Type, td.Result); err != nil {
-				klog.Error("Create message data failed")
-				return
-			}
+		if err := td.handlerPublish(); err != nil {
+			klog.Error("publish data to mq failed")
 		}
-		if err = globals.MqttClient.Publish(td.Topic, payload); err != nil {
-			klog.Error(err)
-		}
-
-		klog.V(2).Infof("Update value: %s, topic: %s", td.Result, td.Topic)
 	}
 }
 
-//ConvertReadData is the function responsible to convert the data read from the device into meaningful data
+func (td *TwinData) handlerPublish() (err error) {
+	// construct payload
+	var payload []byte
+	if strings.Contains(td.Topic, "$hw") {
+		if payload, err = common.CreateMessageTwinUpdate(td.Name, td.Type, td.Result); err != nil {
+			klog.Error("Create message twin update failed")
+			return
+		}
+	} else {
+		if payload, err = common.CreateMessageData(td.Name, td.Type, td.Result); err != nil {
+			klog.Error("Create message data failed")
+			return
+		}
+	}
+	if err = globals.MqttClient.Publish(td.Topic, payload); err != nil {
+		klog.Error(err)
+	}
+
+	klog.V(2).Infof("Update value: %s, topic: %s", td.Result, td.Topic)
+	return
+}
+
+// ConvertReadData is the function responsible to convert the data read from the device into meaningful data
+// If currently logic of Convert data is not suitbale for your deive,you can change ConvertReadData function manually
 func (td *TwinData) ConvertReadData(data []byte) float64 {
 	var intermediateResult uint64
 	var initialValue []byte
