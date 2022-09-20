72e2be02f4fd9a3aa0171de97acf197e914a72e1
WangQing-WUT <1442459023@qq.com>
2022-09-21 04:10:23 +0800

    init gige mapper
    Signed-off-by: WangQing-WUT <1442459023@qq.com>

--- mappers/gige/cmd/main.go
+++ mappers/gige/cmd/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"github.com/kubeedge/mappers-go/mapper-sdk-go/pkg/service"
-	"github.com/kubeedge/mappers-go/mappers/gige-sdk/driver"
+	"github.com/kubeedge/mappers-go/mappers/gige/driver"
 )
 
 func main() {
